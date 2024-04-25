cb73c681779e934c900eb34967d692ef3c71423e
tushcmd <muturidavid854@gmail.com>
2024-04-25 15:20:26 +0300

    README

---
+++ .history/app/page_20240424212022.tsx
@@ -0,0 +1,43 @@
+import Main from '@/components/Main/Main';
+import { getData } from './getData';
+import { Country } from '@/types/item-types';
+
+export default async function Page() {
+  const countries = await getData();
+  return (
+    <main className="page-container">
+      <ServerMain countries={countries} />
+    </main>
+  );
+}
+
+const ServerMain = async ({ countries }: { countries: Country[] }) => {
+  const searchParams = new URLSearchParams();
+  searchParams.set('countries', JSON.stringify(countries));
+  return <Main searchParams={searchParams} />;
+};
+
+export { ServerMain };
+
+
+{/*
+import Main from '@/components/Main/Main';
+import { getData } from './getData';
+import { Country } from '@/types/item-types';
+
+export default async function Page() {
+  const countries = await getData();
+  return (
+    <main className="page-container">
+      <ServerMain countries={countries} />
+    </main>
+  );
+}
+
+const ServerMain = ({ countries }: { countries: Country[] }) => {
+  return <Main countries={countries} />;
+};
+
+export { ServerMain };
+
+*/}
--- .history/components/Main/Main_20240424211832.tsx
+++ .history/components/Main/Main_20240424211832.tsx
@@ -1,46 +1,3 @@
-'use client';
-
-import { useState } from 'react';
-import CountriesTable from '@/components/CountriesTable/CountriesTable';
-import SearchInput from '@/components/SearchInput/SearchInput';
-import { Country } from '@/types/item-types';
-
-interface MainProps {
-  countries: Country[];
-}
-
-export default function Main() {
-  
-  const [countries, setCountries] = useState<Country[]>([]);
-  const [keyword, setKeyword] = useState('');
-
-  
-  const filteredCountries = countries?.length ? countries.filter(
-    (country) =>
-      country.name.toLowerCase().includes(keyword) ||
-      country.region.toLowerCase().includes(keyword) ||
-      country.subregion.toLowerCase().includes(keyword)
-  ) : [];
-
-  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setKeyword(e.target.value.toLowerCase());
-  };
-
-  return (
-    <>
-      {/* No of countries + InputContainer */}
-      <div className="flex justify-between items-center">
-        {/* No of countries */}
-        <div>{countries.length} countries</div>
-        <SearchInput onChange={onInputChange} />
-      </div>
-      <CountriesTable countries={filteredCountries} />
-    </>
-  );
-}
-
-
 {/*
 'use client';
 
@@ -88,3 +45,44 @@ export default function Main({ searchParams }: MainProps) {
 //}
 
 
+'use client';
+
+import { useState } from 'react';
+import CountriesTable from '@/components/CountriesTable/CountriesTable';
+import SearchInput from '@/components/SearchInput/SearchInput';
+import { Country } from '@/types/item-types';
+
+interface MainProps {
+  countries: Country[];
+}
+
+export default function Main() {
+  
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [keyword, setKeyword] = useState('');
+
+  
+  const filteredCountries = countries?.length ? countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  ) : [];
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setKeyword(e.target.value.toLowerCase());
+  };
+
+  return (
+    <>
+      {/* No of countries + InputContainer */}
+      <div className="flex justify-between items-center">
+        {/* No of countries */}
+        <div>{countries.length} countries</div>
+        <SearchInput onChange={onInputChange} />
+      </div>
+      <CountriesTable countries={filteredCountries} />
+    </>
+  );
+}
